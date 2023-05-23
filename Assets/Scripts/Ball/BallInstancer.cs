a8d9720ffbe706107b22e20790f46c0b08cde810
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-05-23 13:06:10 +0200

    Added White ball
    added the white ball basic instanation into the ballInstancer

--- Assets/Scripts/Ball/BallInstancer.cs
+++ Assets/Scripts/Ball/BallInstancer.cs
@@ -29,10 +29,11 @@ public class BallInstancer : MonoBehaviour
     [Space]
 
     [SerializeField] Transform triangleStartingTransform;
+    [SerializeField] Transform whiteStatingTransform;
 
     GameObject ball;
 
-    BallManager ballManager;                               // * Maybe inherht form ball manager in the furure *
+    BallManager ballManager;                     // * Maybe inherht form ball manager in the furure *
 
     float  instanceOffsetY = 1.1f;
     float instanceOffsetX = 1.8f;
@@ -40,6 +41,8 @@ public class BallInstancer : MonoBehaviour
     private void Start()
     {
         ballManager = new (15);
+
+        Instantiate(whiteBall, whiteStatingTransform);
 
         InstanceBalls();
     }
@@ -84,63 +87,63 @@ public class BallInstancer : MonoBehaviour
         switch(numInOrder)
         {
             case 0:
-                ball = Instantiate(fillBallNumOne, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(fillBallNumOne, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 1:
-                ball = Instantiate(fillBallNumTwo, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(fillBallNumTwo, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 2:
-                ball = Instantiate(fillBallNumThree, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(fillBallNumThree, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 3:
-                ball = Instantiate(fillBallNumFour, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(fillBallNumFour, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 4:
-                ball = Instantiate(fillBallNumFive, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(fillBallNumFive, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 5:
-                ball = Instantiate(fillBallNumSix, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(fillBallNumSix, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 6:
-                ball = Instantiate(fillBallNumSeven, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(fillBallNumSeven, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 7:
-                ball = Instantiate(eightBall, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(eightBall, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 8:
-                ball = Instantiate(stripeBallNumOne, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(stripeBallNumOne, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 9:
-                ball = Instantiate(stripeBallNumTwo, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(stripeBallNumTwo, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 10:
-                ball = Instantiate(stripeBallNumThree, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(stripeBallNumThree, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 11:
-                ball = Instantiate(stripeBallNumFour, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(stripeBallNumFour, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 12:
-                ball = Instantiate(stripeBallNumFive, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(stripeBallNumFive, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 13:
-                ball = Instantiate(stripeBallNumSix, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(stripeBallNumSix, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             case 14:
-                ball = Instantiate(stripeBallNumSeven, spawnLocation.transform.position, spawnLocation.transform.rotation);
+                ball = Instantiate(stripeBallNumSeven, spawnLocation);
                 ball.GetComponent<BallLogic>().PassBallIndentity(ballManager.balls[numInOrder]);
                 break;
             default:
