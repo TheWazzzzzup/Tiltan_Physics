2b429eae19b3e3a0d875441cfb5c27a0f5288fb4
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-06-13 19:41:10 +0200

    Drag and Gradual Stop Fix
    made it so the drag is now a number easier to work with
    
    also fixed the drag hiccup by few if statemeants

--- Assets/Scripts(Amit'sPhysics)/RigidAmitComponent.cs
+++ Assets/Scripts(Amit'sPhysics)/RigidAmitComponent.cs
@@ -11,10 +11,11 @@ public class RigidAmitComponent : MonoBehaviour
     // the current velocity of the rigidamit component
     [SerializeField] Vector2 Velocity;
 
-
     [SerializeField] float Drag;
 
     Vector2 calculationsVector;
+    
+    float calculatedDrag => Drag/100;
 
     private void Awake()
     {
@@ -28,23 +29,23 @@ public class RigidAmitComponent : MonoBehaviour
        // TODO: Fix the stop bounce this method creates // When close to a stop, this make the object come to a stop faster
         if (Mathf.Abs(Velocity.magnitude) <= 0.1f)
         {
-            calculationsVector = Velocity * (Drag * 5);
-            transform.position = positionOnLastUpdate.position + (Vector3)(Velocity - calculationsVector);
-            Velocity -= calculationsVector;
-            Debug.Log(Velocity.magnitude);
+            calculationsVector = Velocity * (calculatedDrag * 5);
         }
 
         // This is a thershold that will stop the object from moving if reached
+
+        else
+        {
+            calculationsVector = Velocity * calculatedDrag;
+        }
+
+        transform.position = positionOnLastUpdate.position + (Vector3)(Velocity - calculationsVector);
+        Velocity -= calculationsVector;
+
         if (Mathf.Abs(Velocity.magnitude) <= 0.001f)
         {
             Velocity = Vector2.zero;
         }
-
-        // the basic calculation of a gradual stop // need to make this into a method !
-        calculationsVector = Velocity * Drag;
-        transform.position = positionOnLastUpdate.position + (Vector3)(Velocity - calculationsVector);
-        Velocity -= calculationsVector; 
-       
 
         positionOnLastUpdate.position = transform.position;
     }
